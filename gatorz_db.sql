23cb3ebc36393dcde87131b54a95bcd19523b313
Joseph Salmon <164256139+Sjoseph9652@users.noreply.github.com>
2025-02-05 19:54:11 -0600

    Add files via upload

--- gatorz_db.sql
+++ gatorz_db.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Feb 01, 2025 at 04:48 PM
+-- Host: 127.0.0.1
+-- Generation Time: Feb 06, 2025 at 02:53 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -37,6 +37,15 @@ CREATE TABLE `dances` (
   `image` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `dances`
+--
+
+INSERT INTO `dances` (`dance_ID`, `name`, `region`, `style`, `description`, `status`, `image`) VALUES
+(1, 'Tango', 'Mexico', 'Tango', 'Dance', 0, ''),
+(2, 'example dance', 'Mexico', 'Southern', 'This is an example dance', 0, ''),
+(3, 'example dance', 'Mexico', 'Western', 'This is an example dance', 0, '');
+
 -- --------------------------------------------------------
 
 --
@@ -64,6 +73,15 @@ CREATE TABLE `users` (
   `user_password` varchar(30) NOT NULL,
   `user_role` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_ID`, `first_name`, `last_name`, `email`, `user_password`, `user_role`) VALUES
+(1, 'Joe ', 'Salmon ', 'sv2606xg@go.minnstate.edu', 'password', 'user'),
+(2, 'example ', 'account', 'example.account@gmail.com', '1234', 'user'),
+(3, 'Bill ', 'Bob', 'Bill.Bob@gmail.com', '4321', 'user');
 
 --
 -- Indexes for dumped tables
@@ -95,7 +113,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `dances`
 --
 ALTER TABLE `dances`
-  MODIFY `dance_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `dance_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `preferences`
@@ -107,7 +125,7 @@ ALTER TABLE `preferences`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
