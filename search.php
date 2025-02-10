4894299ccd638d2cac44a106fe865a985ff2f49f
CoryKeuning1 <ri4118rs@go.minnstate.edu>
2025-02-10 08:17:39 -0600

    Update search.php
    Added form action and area for search results, plus some line spacing.

--- search.php
+++ search.php
@@ -7,14 +7,24 @@
 </head>
 <body>
     <h2>Search</h2>
-    <label for="search">Search by:</label>
-    <div>
-        <button type="button">Name</button>
-        <button type="button">Region</button>
-        <button type="button">Style</button>
-    </div>
-    <div>
-        <input type="text" id="search" name="search" placeholder="Enter search...">
+    <form action="search.php" method="POST">
+        <label for="search">Search by:</label>
+        <br><br>
+        <div>
+            <button type="button">Name</button>
+            <button type="button">Region</button>
+            <button type="button">Style</button>
+        </div>
+        <br>
+        <div>
+            <input type="text" id="search" name="search" placeholder="Enter search...">
+            <button type="submit">Search</button>
+        </div>
+    </form>
+    <br>
+    <div id="results" style="border: 1px solid #000; padding: 10px; min-height: 50px; 
+        max-height: 200px; overflow-y: auto;"> 
+        <!-- Search results will appear here -->
     </div>
 </body>
 </html>
