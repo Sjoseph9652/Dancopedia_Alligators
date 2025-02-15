1b6e51405dc1d7ad40f250656ba85e368ffed21f
CoryKeuning1 <ri4118rs@go.minnstate.edu>
2025-02-15 07:23:42 -0600

    create login_form_handler.php and changed LoginForm.php

--- LoginForm.php
+++ LoginForm.php
@@ -27,7 +27,7 @@ ob_flush();
         <div class="tab-content">
             <div id="login" class="tab-pane fade show active">
                 <h2>Welcome Back!</h2>
-                <form action="loginForm.php" method="post">
+                <form action="login_form_handler.php" method="post">
                     <div class="mb-3">
                         <label>Email Address</label>
                         <input type="email" class="form-control" required name="email">
