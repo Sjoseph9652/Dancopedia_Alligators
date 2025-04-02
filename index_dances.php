4d9e27ed6b20562fe13c9df5d3299d041d34205a
Joe-Kroening <zh0865qo@go.minnstate.edu>
2025-04-02 16:14:08 -0500

    Chnaged CSS and added added videos to home page IF they were in the database to display over the pictures

--- index_dances.php
+++ index_dances.php
@@ -7,7 +7,7 @@ $username = 'root';
 $password = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); #;port=3308
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
