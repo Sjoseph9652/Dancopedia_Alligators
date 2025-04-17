826b3e1b8a58c3d5d4ceb2240381fd75357b05a5
Joseph Salmon <164256139+Sjoseph9652@users.noreply.github.com>
2025-04-17 18:33:14 -0500

    Update router.php

--- router.php
+++ router.php
@@ -1,9 +1,1 @@
-<?php
-if (php_sapi_name() == 'cli-server') {
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
-require_once 'index.php';
+
